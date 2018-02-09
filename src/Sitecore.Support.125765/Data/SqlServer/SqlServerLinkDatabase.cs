d3f765dcacb9b113bff986b5d6d605c409903d49
DK\dmsa <dmsa@sitecore.net>
2018-02-09 18:23:51 +0200

    Fix the Links update performance issue.

---
+++ src/Sitecore.Support.125765/Data/SqlServer/SqlServerLinkDatabase.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Data.SqlServer;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SqlServerLinkDatabaseOrigin = Sitecore.Data.SqlServer.SqlServerLinkDatabase;
+
+namespace Sitecore.Support.Data.SqlServer
+{
+  public class SqlServerLinkDatabase : SqlServerLinkDatabaseOrigin
+  {
+    public SqlServerLinkDatabase(string connectionString) : base(connectionString) { }
+
+    public override void UpdateItemVersionReferences(Item item)
+    {
+      Assert.ArgumentNotNull(item, "item");
+
+      Task.Factory.StartNew(() =>
+      {
+        var links = item.Links.GetAllLinks(false);
+        this.UpdateItemVersionLink(item, links);
+      });
+    }
+  }
+}
